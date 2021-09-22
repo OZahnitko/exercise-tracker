dc75e360a2265165556a7ee034dad702db6d3df2
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-09-22 15:23:20 +1000

    locallly foraged

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,36 @@
+import localforage from "localforage";
+
 const App = () => (
   <div className="App">
     <h1>Hello from the other side.</h1>
+    <button
+      onClick={async () => {
+        try {
+          await localforage.setItem("exercises", [
+            "dead lift",
+            "bench press",
+            "squat",
+          ]);
+        } catch (err) {
+          console.log(err);
+        }
+      }}
+    >
+      SAVE in local forage
+    </button>
+
+    <button
+      onClick={async () => {
+        try {
+          const exercises = await localforage.getItem("exercises");
+          console.log(exercises);
+        } catch (err) {
+          console.log(err);
+        }
+      }}
+    >
+      GET IT OUT OF THERE
+    </button>
   </div>
 );
 
