65677f781eb60985e07149b9ce894101a9e71384
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-09-23 12:58:10 +1000

    better local storage workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,31 @@ import LuxonUtils from "@date-io/luxon";
 import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { Button, TextField } from "@material-ui/core";
 import { DateTime } from "luxon";
+import { useEffect } from "react";
 
 import { getCurrentDate, getSelectedDate, setSelectedDate } from "./appSlice";
 import { useAppSelector, useAppDispatch } from "./store";
 import { RootWrapper, TextFieldWrapper } from "./Styles";
+import { checkLocalStorage } from "./utility";
 
 const App = () => {
   const currentDate = useAppSelector(getCurrentDate);
   const selectedDate = useAppSelector(getSelectedDate);
 
   const dispatch = useAppDispatch();
+
+  const checkLocal = async () => {
+    try {
+      await checkLocalStorage();
+    } catch (err) {
+      console.log("There was an error");
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    checkLocal();
+  }, []);
 
   return (
     <RootWrapper>
@@ -21,7 +36,7 @@ const App = () => {
       <Button
         color="primary"
         onClick={() => {
-          dispatch(setSelectedDate(DateTime.now().toISODate()));
+          dispatch(setSelectedDate(DateTime.now().toISO()));
         }}
         variant="contained"
       >
@@ -36,7 +51,7 @@ const App = () => {
             variant="static"
             openTo="date"
             value={selectedDate}
-            onChange={(date) => dispatch(setSelectedDate(date!.toISODate()))}
+            onChange={(date) => dispatch(setSelectedDate(date!.toISO()))}
           />
         </div>
       </MuiPickersUtilsProvider>
--- src/utility/localStorage.ts
+++ src/utility/localStorage.ts
@@ -1,12 +1,24 @@
 import localforage from "localforage";
+import { DateTime } from "luxon";
 
 import { Data } from "../contracts";
+import data from "../data/data.json";
 
-export const storeExercises = async (exercises: string[]) =>
-  await localforage.setItem("exercises", exercises);
+export const checkLocalStorage = async () => {
+  const localData = await fetchData();
 
-export const fetchExercises = async (): Promise<string[] | null> =>
-  await localforage.getItem("exercises");
+  if (!localData) {
+    await storeData(data);
+  } else {
+    //  Need to check if the local date is of the latest version.
+    if (
+      DateTime.fromISO(localData.lastUpdatedOn) <
+      DateTime.fromISO(data.lastUpdatedOn)
+    ) {
+      await storeData(data);
+    }
+  }
+};
 
 export const storeData = async (data: Data) =>
   await localforage.setItem("data", data);
